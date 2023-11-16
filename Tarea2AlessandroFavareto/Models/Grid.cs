d37efe1f0f3f336d5cb45ed5d472f97bf8e12858
AptPage3 <95665618+AptPage3@users.noreply.github.com>
2023-11-16 16:53:31 -0600

    Commit 11/16/2023

---
+++ Tarea2AlessandroFavareto/Models/Grid.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Tarea2AlessandroFavareto.Models
+{
+    public class Grid
+    {
+        [Key]
+        public int Posicion { get; set; }
+        public string Nombre  { get; set; }
+        public int Clasificacion { get; set; }
+        public string Porcentaje { get; set; }
+    }
+}
